5c74a9c3c1de104f41b16eb7f0b61b61a034b5b7
rranlyday <270644101@qq.com>
2016-05-28 21:59:42 +0800

    add

--- src/main/webapp/js/questionDetail.js
+++ src/main/webapp/js/questionDetail.js
@@ -3,10 +3,7 @@
  */
 $(document).ready(function(){
     setTop();
-
 });
-
-
 
 function getUrlParam(name) {
     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
@@ -15,7 +12,6 @@ function getUrlParam(name) {
         return  unescape(r[2]);
     return null;
 }
-
 
 function setTop(){
     var id = getUrlParam("id");
@@ -58,4 +54,120 @@ function setTop(){
         var date = new Date(questionDTO.question.questionTime);
         document.getElementById("#questionTime").innerHTML  = getTimeStr(date);
     }
+}
+
+function showReply(curPage,pageSize){
+    var questionReplyDTOList = null;
+    var id = getUrlParam("id");
+    $.ajax({
+        type : "post",
+        contentType : "application/x-www-form-urlencoded;charset=UTF-8",
+        url : '/questionReplyController/searchByTime.do',
+        async : false,
+        data : {
+            questionId:id,
+            curPage:curPage,
+            pageSize:pageSize
+        },
+        dataType : 'json',
+        success : function(msg) {
+            if (msg.result == true){
+                questionReplyDTOList = msg.questionReplyDTOList;
+            }
+            else{
+                console.log("发布作业失败！")
+            }
+        },error: function(msg){
+            alert("网络超时!");
+        }
+    });
+
+    if(questionReplyDTOList){
+        questionReplyDTOList.forEach(function(questionReplyDTO,index){
+            var template = document.querySelector("#replyTemplate");
+            template.content.querySelector("#replyer").innerHTML  = questionReplyDTO.user.userName;
+            template.content.querySelector("#replyerDesc").innerHTML  =questionReplyDTO.questionReply.replyDescription;
+            template.content.querySelector("#replyTime").innerHTML  = getTimeStr(new Date(questionReplyDTO.questionReply.replyTime));
+            template.content.querySelector("#replyNum").innerHTML  = questionReplyDTO.questionReply.replyNumber;
+            template.content.querySelector("#proNum").innerHTML  = questionReplyDTO.questionReply.proNum;
+            template.content.querySelector("#inverseNum").innerHTML  = questionReplyDTO.questionReply.inverseNum;
+            var  commentContainer = template.content.querySelector(".commentContainer");
+            commentContainer.innerHTML = "";                                   //清空评论
+            commentContainer.id = questionReplyDTO.questionReply.id ;          //为修改埋下伏笔
+            var  comment = showComment(questionReplyDTO.questionReply.id,0,8);
+            if(comment){
+                comment.forEach(function(replyQuestionReplyDTO,index){      //添加评论
+                    var commentTemplate =  document.querySelector("#commentTemplate");
+                    commentTemplate.content.querySelector("#commenter").innerHTML  = replyQuestionReplyDTO.user.userName;
+                    commentTemplate.content.querySelector("#commentDesc").innerHTML  =replyQuestionReplyDTO.replyQuestionReply.replyDescription;
+                    commentTemplate.content.querySelector("#commentTime").innerHTML  = getTimeStr(new Date(replyQuestionReplyDTO.replyQuestionReply.relpyTime));
+                    commentTemplate.content.querySelector("#commentProNum").innerHTML  = replyQuestionReplyDTO.replyQuestionReply.replyProNum;
+                    commentTemplate.content.querySelector("#commentInverseNum").innerHTML  = replyQuestionReplyDTO.replyQuestionReply.replyInverseNum;
+                    commentContainer.appendChild(commentTemplate.content.cloneNode(true));
+                });
+            }
+            document.querySelector("#replyContainer").appendChild(template.content.cloneNode(true));   //加进去
+        });
+    }
+}
+
+function showComment(replyId,curPage,pageSize){
+    var comment = null;
+    $.ajax({
+        type : "post",
+        contentType : "application/x-www-form-urlencoded;charset=UTF-8",
+        url : '/replyQuestionReplyController/searchReplyQuestionReplyOrderByTime.do',
+        async : false,
+        data : {
+            questionReplyId:replyId,
+            curPage:curPage,
+            pageSize:pageSize
+        },
+        dataType : 'json',
+        success : function(msg) {
+            if (msg.result == true){
+                comment = msg.replyQuestionReplyDTOList;
+            }
+            else{
+                console.log("获取评论失败！")
+            }
+        },error: function(msg){
+            alert("网络超时!");
+        }
+    });
+}
+function getTimeStr(date){
+    var year = date.getFullYear();
+    var month = date.getMonth()+1;
+    var day =  date.getDay();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if(month<10)
+        month = "0"+month;
+    if(day<10)
+        day = "0" +day ;
+    if(hours < 10)
+        hours = "0"+hours;
+    if(minutes < 10)
+        minutes = "0"+minutes;
+    if(seconds < 10)
+        seconds = "0"+seconds;
+    return year+"-"+month+"-"+day+" "+hours+":"+minutes+":"+seconds ;
+}
+
+function getDate(date){
+    var year = date.getFullYear();
+    var month = date.getMonth()+1;
+    var day =  date.getDay();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if(month<10)
+        month = "0"+month;
+    if(day<10)
+        day = "0" +day ;
+    return year+"-"+month+"-"+day ;
 }
